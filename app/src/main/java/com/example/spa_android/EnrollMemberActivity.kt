e96537112e9607564c932924c7a2174066e3146c
Junseo <junso0324@naver.com>
2024-07-09 17:12:32 +0900

    7/9

--- app/src/main/java/com/example/spa_android/EnrollMemberActivity.kt
+++ app/src/main/java/com/example/spa_android/EnrollMemberActivity.kt
@@ -1,15 +1,23 @@
 package com.example.spa_android
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.spa_android.databinding.ActivityEnrollMemberBinding
+
 
 class EnrollMemberActivity : AppCompatActivity() {
     private lateinit var binding : ActivityEnrollMemberBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+     // BoardFragment로 이동하는 Intent 생성
+           
+
         binding = ActivityEnrollMemberBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
