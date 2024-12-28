f3540a30262314fca1f1b76f3aa6f47c496f39cf
chaechae0817 <cofbf2017@gmail.com>
2024-12-28 15:18:53 +0900

    Merge pull request #23 from Junseo0324/master
    12/28 commit

--- app/src/main/java/com/example/spa_android/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/spa_android/fragment/HomeFragment.kt
@@ -21,31 +21,32 @@ import retrofit2.Response
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var projectAdapter: ProjectListAdapter
-    private var projectList : ArrayList<ProjectListModel> = ArrayList()
+    private var projectList: ArrayList<ProjectListModel> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val sharedPreferences = requireActivity().getSharedPreferences("MyInformation",Context.MODE_PRIVATE)
-        var email = sharedPreferences.getString("email",null).toString()
+        val sharedPreferences = requireActivity().getSharedPreferences("MyInformation", Context.MODE_PRIVATE)
+        val email = sharedPreferences.getString("email", null).toString()
+
         // 어댑터 생성 및 설정
-        projectAdapter = ProjectListAdapter(projectList, object : ProjectListAdapter.OnItemClickListener{
-            override fun onItemClick(item: String,name: String) {
-                val intent = Intent(requireContext(),ProjectActivity::class.java)
-                intent.putExtra("selectedProject",item)
-                intent.putExtra("selectedProjectName",name)
+        projectAdapter = ProjectListAdapter(projectList, object : ProjectListAdapter.OnItemClickListener {
+            override fun onItemClick(item: String, name: String) {
+                val intent = Intent(requireContext(), ProjectActivity::class.java)
+                intent.putExtra("selectedProject", item)
+                intent.putExtra("selectedProjectName", name)
                 startActivity(intent)
             }
         })
@@ -54,17 +55,15 @@ class HomeFragment : Fragment() {
         binding.projectRv.layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
 
         getProjectList(email)
-
-
     }
 
-    private fun getProjectList(email: String) : ArrayList<ProjectListModel> {
-        RetrofitApplication.networkService.getProjectList(email).clone()?.enqueue(object : Callback<ArrayList<ProjectListModel>>{
+    private fun getProjectList(email: String): ArrayList<ProjectListModel> {
+        RetrofitApplication.networkService.getProjectList(email).clone()?.enqueue(object : Callback<ArrayList<ProjectListModel>> {
             override fun onResponse(call: Call<ArrayList<ProjectListModel>>, response: Response<ArrayList<ProjectListModel>>) {
-                if (response.isSuccessful){
-                    projectList.clear()  // 기존 데이터 삭제
-                    projectList.addAll(response.body()!!)  // 새로운 데이터 추가
-                    projectAdapter.notifyDataSetChanged()  // 어댑터에 데이터 변경 알림
+                if (response.isSuccessful) {
+                    projectList.clear() // 기존 데이터 삭제
+                    projectList.addAll(response.body()!!) // 새로운 데이터 추가
+                    projectAdapter.notifyDataSetChanged() // 어댑터에 데이터 변경 알림
                     Log.d(TAG, "onResponse: ${response.message()}")
                 }
             }
@@ -76,8 +75,8 @@ class HomeFragment : Fragment() {
         })
         return projectList
     }
-    
-    companion object{
+
+    companion object {
         const val TAG = "HomeFragment"
     }
 }
